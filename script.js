9574e81f2ae425ad2036554fc4d1be3c168bbb8c
ajaykumar-28 <80011495+ajaykumar-28@users.noreply.github.com>
2023-12-21 12:31:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,6 +17,30 @@ function search() {
     }
   }
 
+}
 
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
 
+// When scrolls down 500px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 500 ||
+    document.documentElement.scrollTop > 500
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
